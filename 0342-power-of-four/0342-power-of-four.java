b572081074f7bd9296092df9d614ebc53c499501
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-10-27 18:10:32 +0530

    Time: 0 ms (100%), Space: 41 MB (37.48%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0)
+        {
+            return false;
+
+        }
+        else if(n<1)
+        {
+            return false;
+        }
+        else
+        {
+            while(n%4==0)
+            {
+                n=n/4;
+            }
+            if(n==1)
+            {
+                return true;
+
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
