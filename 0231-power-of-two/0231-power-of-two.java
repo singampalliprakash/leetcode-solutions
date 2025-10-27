347b53cd988a9a7f71bfd38a2d7c17853180cef5
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-10-27 17:58:34 +0530

    Time: 0 ms (100%), Space: 41.2 MB (18.65%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)
+        {
+            return false;
+        }
+        else if(n<1)
+        {
+            return false;
+        }
+        else
+        {
+        while(n%2==0)
+        {
+            n=n/2;
+        }
+            if(n==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+        }
+         
+    }
+
+        
+
