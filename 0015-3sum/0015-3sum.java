59f9efd3bd5a92d20c4e8d00f453979345a2f4ab
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-11-15 20:29:53 +0530

    Time: 32 ms (45.17%), Space: 58.9 MB (16.47%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,58 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // Set<List<Integer>> res = new HashSet<>();
+        // Arrays.sort(nums);
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         for (int k = j + 1; k < nums.length; k++) {
+        //             if (nums[i] + nums[j] + nums[k] == 0) {
+        //                 List<Integer> tmp = Arrays.asList(nums[i], nums[j], nums[k]);
+        //                 res.add(tmp);
+        //             }
+        //         }
+        //     }
+        // }
+        // return new ArrayList<>(res);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); 
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            
+            
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    
+                    right--;
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } 
+                else if (sum < 0) {
+                    left++;  
+                } 
+                else {
+                    
+                    right--; 
+                }
+            }
+        }
+        return result;
+
+    }
+}
