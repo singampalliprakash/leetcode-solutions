16512e398184936e278abc1e5f1ba5f3dc7af1fe
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-11-23 12:03:33 +0530

    Time: 2 ms (64.5%), Space: 94.5 MB (14.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy_price=prices[0];
+       int cp=prices[0];
         int profit=0;
         for(int i=1;i<prices.length;i++)
         {
-            if(prices[i]<buy_price)
+            if(prices[i]<cp)
             {
-                buy_price=prices[i];
+                cp=prices[i];
             }
             else
             {
-                int current_profit=prices[i]-buy_price;
-                profit=Math.max(current_profit,profit);
+                int current_profit=prices[i]-cp;
+                profit=Math.max(profit,current_profit);
             }
           
         }
           return profit;
-        
     }
 }
