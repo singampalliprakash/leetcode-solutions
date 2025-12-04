6f5b84eb898f7827a53e307979e9d3a5f236d68c
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-12-04 17:09:57 +0530

    Time: 2 ms (38.52%), Space: 45.3 MB (37.95%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        ArrayList<List<Integer>> res=new ArrayList<>();
+        backTrack(res,new ArrayList<>(),nums);
+        return res;
+    }
+    private void backTrack(List<List<Integer>> res,List<Integer> tempList,int[] nums)
+    {
+        if(tempList.size()==nums.length)
+        {
+            res.add(new ArrayList<>(tempList));
+        }
+
+        else
+        {
+            for(int i=0;i<nums.length;i++)
+            {
+                if(tempList.contains(nums[i]))
+                {
+                    continue;
+                }
+                tempList.add(nums[i]);
+                backTrack(res,tempList,nums);
+                tempList.remove(tempList.size()-1);
+            }
+        }
+    }
+}
