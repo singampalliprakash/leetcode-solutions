cf879d28cf3f690227bc2862252d8d504efa85b6
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2026-01-29 20:26:49 +0530

    Time: 1 ms (99.74%), Space: 47.8 MB (66.72%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+
+        int m = board.length;
+        int n = board[0].length;
+
+       
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);       // Left border
+            dfs(board, i, n - 1);   // Right border
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);       // Top border
+            dfs(board, m - 1, j);   // Bottom border
+        }
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';  
+                } else if (board[i][j] == 'S') {
+                    board[i][j] = 'O';  
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length ||
+        board[i][j] != 'O') {
+            return;
+            }
+
+        board[i][j] = 'S';  
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
