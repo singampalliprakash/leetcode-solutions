eb459fda3de8203d573220ebaaa7d71b5f31e7b5
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2026-03-09 13:15:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
-        k=k%n;
+        // k=k%n;
         reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
