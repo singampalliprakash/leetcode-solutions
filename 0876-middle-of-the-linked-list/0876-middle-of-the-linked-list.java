04dd35d481be8a0fffb87d475ce0159c669bd183
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2026-03-16 17:49:43 +0530

    Time: 0 ms (100%), Space: 42.9 MB (49.47%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,8 +10,24 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int count=0;
+        
+        // int count=0;
+        // ListNode temp=head;
+        // while(temp!=null)
+        // {
+        //     count++;
+        //     temp=temp.next;
+        // }
+
+        // temp=head;
+        // for(int i=0;i<count/2;i++)
+        // {
+        //     temp=temp.next;
+        // }
+        // return temp;
+
         ListNode temp=head;
+        int count=0;
         while(temp!=null)
         {
             count++;
