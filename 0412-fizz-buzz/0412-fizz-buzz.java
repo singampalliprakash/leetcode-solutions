7e57e7eb5f5a3f8f832cef17e73bf5c803fbecb4
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2026-06-11 19:34:35 +0530

    Time: 1 ms (99.89%), Space: 47 MB (27.51%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,24 +3,23 @@ class Solution {
         ArrayList<String> li=new ArrayList<>();
         for(int i=1;i<=n;i++)
         {
-        if(i%3==0&&i%5==0)
-        {
-            li.add("FizzBuzz");
+            if(i%3==0&&i%5==0)
+            {
+                li.add("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                li.add("Fizz");
+            }
+            else if(i%5==0)
+            {
+                li.add("Buzz");
+            }
+            else
+            {
+                li.add(String.valueOf(i));
+            }
         }
-        else if(i%3==0)
-        {
-            li.add("Fizz");
-        }
-        else if(i%5==0)
-        {
-            li.add("Buzz");
-        }
-        else
-        {
-            li.add(Integer.toString(i));
-        }
-        }
-        return li;
-        
+       return li; 
     }
 }
