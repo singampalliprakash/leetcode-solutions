589021757a7a9c1a43279070c546a09608edd775
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2026-06-12 21:03:05 +0530

    Time: 4 ms (47.29%), Space: 69.5 MB (60.9%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,18 +1,16 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double win_avg=0;
+        double avg=0;
         for(int i=0;i<k;i++)
         {
-            win_avg+=nums[i];
+            avg+=nums[i];
         }
-
-        double max_avg=win_avg;
+        double max_avg=avg;
         for(int j=k;j<nums.length;j++)
         {
-            win_avg+=nums[j]-nums[j-k];
-             max_avg=Math.max(max_avg,win_avg);
-
+            avg+=nums[j]-nums[j-k];
+            max_avg=Math.max(avg,max_avg);
         }
-        return max_avg/k;   
+        return max_avg/k;
     }
 }
